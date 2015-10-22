d745add9a2cec57804124d962bff3b30f87ccaa3
Ryan Haase <ryandhaase@gmail.com>
2015-10-22 11:15:05 -0700

    Adding navbar

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -1,25 +1,29 @@
 angular.module('farmCart')
-.controller('NavCtrl', [
+.controller('navCtrl', [
   '$scope',
-  'Auth',
-  function($scope, Auth) {
-    $scope.signedIn = Auth.isAuthenticated;
-    $scope.logout = Auth.logout;
+  function($scope) {
 
-    Auth.currentUser().then(function(user) {
-      $scope.user = user;
-    });
-
-    $scope.$on('devise:new-registration', function(e, user) {
-      $scope.user = user;
-    });
-
-    $scope.$on('devise:login', function(e, user) {
-      $scope.user = user;
-    });
-
-    $scope.$on('devise:logout', function(e, user) {
-      $scope.user = {};
-    });
   },
+
+  // 'Auth',
+  // function($scope, Auth) {
+  //   $scope.signedIn = Auth.isAuthenticated;
+  //   $scope.logout = Auth.logout;
+  //
+  //   Auth.currentUser().then(function(user) {
+  //     $scope.user = user;
+  //   });
+  //
+  //   $scope.$on('devise:new-registration', function(e, user) {
+  //     $scope.user = user;
+  //   });
+  //
+  //   $scope.$on('devise:login', function(e, user) {
+  //     $scope.user = user;
+  //   });
+  //
+  //   $scope.$on('devise:logout', function(e, user) {
+  //     $scope.user = {};
+  //   });
+  // },
 ]);
